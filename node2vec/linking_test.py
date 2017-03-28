dc0f1855f6cbbd084c27683e7590f2726f3e32ba
younith <amber@butv10.com>
2017-03-28 16:06:44 -0400

    added test

---
+++ node2vec/linking_test.py
@@ -0,0 +1,13 @@
+# Sailung Yeung
+# <yeungsl@bu.edu>
+
+import numpy as np
+import random
+
+class Test():
+    def __init__(self, BFSlist, Edgelist, matrix, mapping):
+        self.G = BFSlist
+        self.E = Edgelist
+        self.M = matrix
+        self.MP = mapping
+    def 
